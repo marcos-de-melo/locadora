9c33dff91890678e1c32c0641db1f664d224a69d
Marcos de Melo <marcdmelo@gmail.com>
2023-05-25 14:52:55 -0300

    Aula 2 - Implementando a navegação de conteúdo

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
     <title>LOCADORA DEV</title>
 </head>
 <body>
-    
+    <head>
+        <h1>LOCADORA DEV</h1>
+        <ul>
+            <li><a href="index.php?menu=home">Home</a></li>
+            <li><a href="index.php?menu=videos">Videos</a></li>
+            <li><a href="index.php?menu=categorias">Categorias</a></li>
+        </ul>
+    </head>
+
 </body>
 </html>
