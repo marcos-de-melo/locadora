9d5f3bd9f9ca8afe158322a1d06c86568952867e
Marcos de Melo <marcdmelo@gmail.com>
2023-05-30 17:26:37 -0300

    Aula 5 - Implementando INNER JOIN

--- db/conexao.php
+++ db/conexao.php
@@ -1,9 +1,8 @@
 <?php
-const SERVIDOR = "localhost";
+
+const SERVIDOR = "root";
 const USUARIO = "root";
 const SENHA = "neon321";
 const BANCO = "dblocadora";
 
 $conexao = mysqli_connect(SERVIDOR, USUARIO, SENHA, BANCO) or die("Erro ao conectar ao servidor. " . mysqli_connect_error());
-
-?>
--- lista-videos.php
+++ lista-videos.php
@@ -1,1 +1,31 @@
 <h2>Lista de Vídeos</h2>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>Titulo</th>
+            <th>Duração do Filme</th>
+            <th>Valor da Locação</th>
+            <th>Categoria</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $sql = "SELECT * FROM tbfilmes";
+        $rs = mysqli_query($conexao, $sql);
+        while ($dados = mysqli_fetch_assoc($rs)) {
+        ?>
+            <tr>
+                <td><?=$dados["idFilme"]?></td>
+                <td><?=$dados["tituloFilme"]?></td>
+                <td><?=$dados["duracaoFilme"]?></td>
+                <td><?=$dados["valorLocacao"]?></td>
+                <td><?=$dados["idCategoria"]?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+
+</table>
