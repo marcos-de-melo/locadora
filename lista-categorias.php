53b49c03b2faad758cfaae3efdd71b11058ab1d1
Marcos de Melo <marcdmelo@gmail.com>
2023-06-01 16:43:28 -0300

    Aula 7 - Cadastro de Filmes

--- lista-categorias.php
+++ lista-categorias.php
@@ -1,1 +1,27 @@
 <h2>Lista de Categorias</h2>
+
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>Nome da Categoria</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $sql = "SELECT * FROM tbcategorias order by nomeCategoria asc";
+        $rs = mysqli_query($conexao, $sql);
+        while ($dados = mysqli_fetch_assoc($rs)) {
+        ?>
+            <tr>
+                <td><?= $dados["idCategoria"] ?></td>
+                <td><?= $dados["nomeCategoria"] ?></td>
+
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+
+</table>
